f62d4b1581c5b4a2f401d703037fb3586608a68c
jawad-zh <zahraoui.jawad20@gmail.com>
2026-02-15 19:22:47 +0000

    .

---
+++ frontend/services/checkCreatPost.js
@@ -0,0 +1,5 @@
+export async function checkCreatPost() {
+        const title = document.getElementById('creatPostTitle').value.trim()
+    const content = document.getElementById('creatPostContent').value.trim()
+   var categoriesChecked =document.querySelectorAll("#creatPostCategories input:checked")
+}
