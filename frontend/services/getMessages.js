be86582640d5dfee73ae250997e9e7acb7dd748e
unknown <fadlo.hosaily@gmail.com>
2026-02-27 15:56:14 +0000

    getMessages

--- frontend/services/getMessages.js
+++ frontend/services/getMessages.js
@@ -1,13 +1,24 @@
-
-export async function getMessages(reiverId) {
-    let receiverID = reiverId
-    let res = await fetch(`/getMessages?receiverID=${receiverID}`, {
+export let messagesoffset = 0
+let lastUserID = null
+export async function getMessages(reiverId,flag) {
+    if(flag !== 'scroll'){messagesoffset=0}
+    if (reiverId !== Number(lastUserID)) {        
+        messagesoffset = 0
+        lastUserID = reiverId
+    }
+    let res = await fetch(`/getMessages?receiverID=${reiverId}&offset=${messagesoffset}`, {
         method: "GET",
         headers: {
             "Content-Type": "application/json"
         }
     })
     var data = await res.json()
+    if (data && data.length !== 0) {
+        messagesoffset += data.length        
+    }    
     return data
-
+}
+export function  addMessageOffset(){
+    messagesoffset+=1
+    
 }
