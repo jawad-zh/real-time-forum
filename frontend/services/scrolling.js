8a6c05f9e56913d050f2221d046d47a4f260c7bd
jawad-zh <zahraoui.jawad20@gmail.com>
2026-02-28 03:18:42 +0000

    fix scrolling event

--- frontend/services/scrolling.js
+++ frontend/services/scrolling.js
@@ -1,16 +1,29 @@
 import {loadPosts} from "/frontend/views/loadPosts.js"
 import { loadMessages } from "/frontend/views/loadMessages.js";
-export function scrollTracking(container,category){
-    container.onscroll = null 
-    container.onscroll = ()=>{
+let isLoading = false
+
+export function scrollTracking( category) {
+    let container = document.getElementById('middle')
+    console.log('container',container);
+    console.log('container height:',container.clientHeight);
+    
+    
+    console.log('999999999999999999999999999999.6953120.65231.0653120.563120');
+    
+    container.onscroll = async () => {
+        if (container.clientHeight === container.scrollHeight) return
+        if (isLoading) return
+
         const scrollTop = container.scrollTop
         const visibleHeight = container.clientHeight
         const fullHeight = container.scrollHeight
-        if (scrollTop + visibleHeight >= fullHeight ) {
-            loadPosts(category,'scroll')
+
+        if (scrollTop + visibleHeight >= fullHeight) {
+            isLoading = true
+            await loadPosts(category, 'scroll')
+            isLoading = false
         }
     }
-    
 }
 export function messageScrolling(container, receiverID) {
     container.addEventListener('scroll', async () => {
