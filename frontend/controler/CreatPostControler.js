038d6d5c270d78ee3e0a3effffe5d96e9bc0c438
jawad-zh <zahraoui.jawad20@gmail.com>
2026-02-28 06:24:23 +0000

    creat post controler

---
+++ frontend/controler/CreatPostControler.js
@@ -0,0 +1,28 @@
+// /frontend/controler/CreatePostController.js
+import { setCreatPost } from "/frontend/views/creatPost.js"
+import { creatPost } from "/frontend/services/creatPost.js"
+import { checkCreatPost } from "/frontend/services/checkCreatPost.js"
+import { removeCreatPostPage } from '/frontend/views/removeCreatPostPage.js'
+import { setAlert } from "/frontend/components/alert.js"
+import { active } from "/frontend/views/active.js"
+
+export async function CreatePostController(e) {
+    const el = e.target.closest('[id]')
+    if (!el) return
+    const id = el.id
+
+    if (id === 'creatPostIcone') {
+        setCreatPost()
+        active(id)
+    } else if (id === 'cancelCreatPost' || id === 'creatPostCountainer') {
+        removeCreatPostPage()
+        active('homePageIcone')
+    } else if (id === 'creatPostButton') {
+        const message = checkCreatPost()
+        if (message === 'success') {
+            await creatPost(e)
+        } else {
+            setAlert('error', '✖', message)
+        }
+    }
+}
