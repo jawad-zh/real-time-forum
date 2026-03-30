2d63f59d6e6c19c4befb8efca0f39192b37381e7
unknown <fadlo.hosaily@gmail.com>
2026-03-30 16:39:00 +0100

    like post

--- backend/repos/likePost.go
+++ backend/repos/likePost.go
@@ -9,43 +9,43 @@ import (
 	"golang/backend/models"
 )
 
-func LikePost(PostID int, UserID int) (error,string,int){
-	fmt.Println("postID from likePost",PostID)
+func LikePost(PostID int, UserID int) (error, string, int) {
+	fmt.Println("postID from likePost", PostID)
 	var id int
 	var potLike models.PostLike
-	err:= db.DataBase.QueryRow(`
+	err := db.DataBase.QueryRow(`
 	SELECT * FROM Posts WHERE PostID = ?
 	`, PostID).Scan(&id)
 	if err == sql.ErrNoRows {
-		// need to do action 
+		// need to do action
 		fmt.Println("the post not exist")
-		return err ,"Post not exist",http.StatusOK
+		return err, "Post not exist", http.StatusNotFound
 	}
-	err= db.DataBase.QueryRow(`
+	err = db.DataBase.QueryRow(`
 	SELECT * FROM PostLike WHERE PostID = ? AND UserID = ?
-	`, PostID,UserID).Scan(&potLike.PostID,&potLike.UserID)
+	`, PostID, UserID).Scan(&potLike.PostID, &potLike.UserID)
 	if err == sql.ErrNoRows {
 
-		_,err=db.DataBase.Exec(`
+		_, err = db.DataBase.Exec(`
 	INSERT INTO PostLike (UserID,PostID)
 	VALUES (?,?)
-	`,UserID,PostID)
-	if err != nil{
-		fmt.Println("insert like error:",err)
-		return err , "like failed",http.StatusInternalServerError
-	}
-	return  nil ,"like success",http.StatusOK
-	}else if err == nil{
-		_,err = db.DataBase.Exec(`
-		DELETE FROM PostLike WHERE UserID = ? AND PostID = ?
-		`,UserID,PostID)
-		if err != nil{
-			fmt.Println("Delet Error:",err)
-			return err , "like failed",http.StatusInternalServerError
+	`, UserID, PostID)
+		if err != nil {
+			fmt.Println("insert like error:", err)
+			return err, "like failed", http.StatusInternalServerError
 		}
-		return nil,"deslike success",http.StatusOK
-	}else{
-		fmt.Println("errrrrrrrrrrrrrrrrrror",err)
-		return err ,"like failed",http.StatusInternalServerError
+		return nil, "like success", http.StatusOK
+	} else if err == nil {
+		_, err = db.DataBase.Exec(`
+		DELETE FROM PostLike WHERE UserID = ? AND PostID = ?
+		`, UserID, PostID)
+		if err != nil {
+			fmt.Println("Delet Error:", err)
+			return err, "like failed", http.StatusInternalServerError
+		}
+		return nil, "deslike success", http.StatusOK
+	} else {
+		fmt.Println("errrrrrrrrrrrrrrrrrror", err)
+		return err, "like failed", http.StatusInternalServerError
 	}
 }
