e677b6cea48555058a86acf1a36f0a1a4048e236
unknown <fadlo.hosaily@gmail.com>
2026-03-30 16:41:40 +0100

    edit profile

--- backend/services/editProfile.go
+++ backend/services/editProfile.go
@@ -12,21 +12,21 @@ import (
 	"golang/backend/repos"
 )
 
-func EditProfile(r *http.Request,UserID int) (error , string , int){
-	err:=repos.CheckUserExist(UserID)
-	if err != nil{
-		return err,"",http.StatusBadGateway
+func EditProfile(r *http.Request, UserID int) (error, string, int) {
+	err := repos.CheckUserExist(UserID)
+	if err != nil {
+		return err, "", http.StatusBadGateway
 	}
-	
+
 	err = r.ParseMultipartForm(10 << 20)
 	if err != nil {
 		fmt.Println("large size")
-		return errors.New("larg image size") , "image too large",http.StatusBadRequest
+		return errors.New("larg image size"), "image too large", http.StatusBadRequest
 	}
 	err = os.MkdirAll("frontend/uploads", os.ModePerm)
 	if err != nil {
 		fmt.Println("failed to create uploads folder: ", err)
-		return err , " edit profile failed try later ",http.StatusBadRequest
+		return err, " edit profile failed try later ", http.StatusBadRequest
 	}
 
 	file, handler, err := r.FormFile("image")
@@ -40,22 +40,22 @@ func EditProfile(r *http.Request,UserID int) (error , string , int){
 		dst, err := os.Create(imagePath)
 		if err != nil {
 			fmt.Println("os Error:", err)
-			return err , "edit profile failed try later" , http.StatusBadRequest
+			return err, "edit profile failed try later", http.StatusBadRequest
 		}
 		defer dst.Close()
 
 		io.Copy(dst, file)
 
 		ImageURL := imagePath
-		err,statueCode := repos.EditProfile(ImageURL, UserID)
+		err, statueCode := repos.EditProfile(ImageURL, UserID)
 		if err != nil {
-			return err , "edit profile failed try later",statueCode
+			return err, "edit profile failed try later", statueCode
 		}
 
-		return nil , "success",http.StatusOK
+		return nil, "success", http.StatusOK
 
 	} else {
 		fmt.Println("somthing wrong")
-		return err , "edit profile failed try later",http.StatusInternalServerError
+		return err, "edit profile failed try later", http.StatusInternalServerError
 	}
 }
