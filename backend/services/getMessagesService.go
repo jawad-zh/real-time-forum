4f35c9d876b504920d76126d4ca7578ce492e4e3
unknown <fadlo.hosaily@gmail.com>
2026-03-30 16:43:07 +0100

    get msj

--- backend/services/getMessagesService.go
+++ backend/services/getMessagesService.go
@@ -6,14 +6,14 @@ import (
 	"net/http"
 )
 
-func GetMessages(UserID int, receiverID int, offset int) (error, *[]models.PrivateMessage ,int ) {
-	err:= repos.CheckUserExist(UserID)
-	if err != nil{
-		return err,nil,http.StatusBadRequest
-	}
-	err, data,statueCode := repos.GetMessagesRepos(receiverID, UserID, offset)
+func GetMessages(UserID int, receiverID int, offset int) (error, *[]models.PrivateMessage, int) {
+	err := repos.CheckUserExist(UserID)
 	if err != nil {
-		return err, nil,statueCode
+		return err, nil, http.StatusBadRequest
 	}
-	return nil, data , statueCode
+	err, data, statueCode := repos.GetMessagesRepos(receiverID, UserID, offset)
+	if err != nil {
+		return err, nil, statueCode
+	}
+	return nil, data, statueCode
 }
