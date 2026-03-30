b49116329b26b86513d75845731abd70d0e6fa3d
unknown <fadlo.hosaily@gmail.com>
2026-03-30 16:47:43 +0100

    send message

--- backend/wbs/sendMessage.go
+++ backend/wbs/sendMessage.go
@@ -1,11 +1,13 @@
 package wbs
 
 import "fmt"
-type SendMessage struct{
+
+type SendMessage struct {
 	MessageContent string `json:"Message"`
-	UserNickname string `json:"Name"`
+	UserNickname   string `json:"Name"`
 }
-func (m *Manager) SendMessage(SenderID int, ReceiverID int, MessageContent string,userNickname string) {
+
+func (m *Manager) SendMessage(SenderID int, ReceiverID int, MessageContent string, userNickname string) {
 	m.Lock()
 	defer m.Unlock()
 	var messageData SendMessage
@@ -16,22 +18,22 @@ func (m *Manager) SendMessage(SenderID int, ReceiverID int, MessageContent strin
 	messageData.MessageContent = MessageContent
 	messageData.UserNickname = userNickname
 	Event.Load = messageData
-	senderClients , ok := m.Clients[SenderID]
+	senderClients, ok := m.Clients[SenderID]
 	if !ok {
 		fmt.Println("Sender connection not found")
 		return
 	}
-	for _,client:= range senderClients{
+	for _, client := range senderClients {
 		client.Conn.WriteJSON(Event)
 	}
 	receiverClients, ok := m.Clients[ReceiverID]
-		fmt.Println("receiverClients Client:",senderClients)
+	fmt.Println("receiverClients Client:", senderClients)
 
 	if !ok {
 		fmt.Println("Connection not found")
 		return
 	}
-		for _, client := range receiverClients {
-			client.Conn.WriteJSON(Event)
-		}
+	for _, client := range receiverClients {
+		client.Conn.WriteJSON(Event)
+	}
 }
