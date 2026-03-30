b42fef3ca01c1da5718e125ba699e787c51e6c70
unknown <fadlo.hosaily@gmail.com>
2026-03-30 16:44:28 +0100

    logout

--- backend/services/logoutService.go
+++ backend/services/logoutService.go
@@ -6,14 +6,14 @@ import (
 	"golang/backend/repos"
 )
 
-func LogoutService(r *http.Request) (error,int,int) {
+func LogoutService(r *http.Request) (error, int, int) {
 	coockie, err := r.Cookie("session_id")
 	if err != nil {
-		return err,0,http.StatusBadRequest
+		return err, 0, http.StatusBadRequest
 	}
-	ok,deletedUserID := repos.DeletSession(coockie.Value)
+	ok, deletedUserID := repos.DeletSession(coockie.Value)
 	if !ok {
-		return err,0,http.StatusInternalServerError
+		return err, 0, http.StatusInternalServerError
 	}
-	return nil,deletedUserID,http.StatusOK
+	return nil, deletedUserID, http.StatusOK
 }
