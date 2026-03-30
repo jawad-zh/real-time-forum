5b2bf5e17b63a674167efd78d868890069a5f01b
unknown <fadlo.hosaily@gmail.com>
2026-03-30 16:45:08 +0100

    save post

--- backend/services/savePostService.go
+++ backend/services/savePostService.go
@@ -7,15 +7,15 @@ import (
 	"golang/backend/repos"
 )
 
-func SavePostService(UserID int, postID int) (error, string,int) {
-	err:= repos.CheckExist(postID)
-	if err != nil{
-		return err , "" ,http.StatusBadRequest
+func SavePostService(UserID int, postID int) (error, string, int) {
+	err := repos.CheckExist(postID)
+	if err != nil {
+		return err, "", http.StatusBadRequest
 	}
-	err, message ,statueCode:= repos.SavePost(postID, UserID)
+	err, message, statueCode := repos.SavePost(postID, UserID)
 	if err != nil {
 		fmt.Println("Erorrrrrrrrr", err)
-		return err, message,statueCode
+		return err, message, statueCode
 	}
-	return nil, message,statueCode
+	return nil, message, statueCode
 }
