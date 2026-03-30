c75e318e040ff3446b1caf0eab015bf035003b0b
unknown <fadlo.hosaily@gmail.com>
2026-03-30 16:17:47 +0100

    get comment and get messages service

--- backend/services/getMessagesService.go
+++ backend/services/getMessagesService.go
@@ -3,9 +3,14 @@ package services
 import (
 	"golang/backend/models"
 	"golang/backend/repos"
+	"net/http"
 )
 
 func GetMessages(UserID int, receiverID int, offset int) (error, *[]models.PrivateMessage ,int ) {
+	err:= repos.CheckUserExist(UserID)
+	if err != nil{
+		return err,nil,http.StatusBadRequest
+	}
 	err, data,statueCode := repos.GetMessagesRepos(receiverID, UserID, offset)
 	if err != nil {
 		return err, nil,statueCode
