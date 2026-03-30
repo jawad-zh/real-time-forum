0df830eda188865ccda283aa0858ac55ce8d7c4c
jawad-zh <zahraoui.jawad20@gmail.com>
2026-03-30 23:37:46 +0100

    .

--- backend/services/UpdateMessageStateService.go
+++ backend/services/UpdateMessageStateService.go
@@ -3,9 +3,14 @@ package services
 import (
 	"golang/backend/repos"
 	"golang/backend/wbs"
+	"net/http"
 )
 
 func UpdateMessageStateServie(UserID int, ReceiverID int) (error, int) {
+	err:=repos.CheckExist(ReceiverID)
+	if err != nil{
+		return err,http.StatusBadRequest
+	}
 	err, statueCode := repos.UpdateMessageStateRepos(UserID, ReceiverID)
 	if err != nil {
 		return err, statueCode
