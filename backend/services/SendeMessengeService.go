ab2641c262afc9d96d00b79ad45e098c8baf8a02
unknown <fadlo.hosaily@gmail.com>
2026-03-30 16:45:21 +0100

    send message

--- backend/services/SendeMessengeService.go
+++ backend/services/SendeMessengeService.go
@@ -2,25 +2,24 @@ package services
 
 import (
 	"errors"
-	"fmt"
+	"net/http"
+
 	"golang/backend/models"
 	"golang/backend/repos"
-	"net/http"
 )
 
 func SendeMessageService(messageInfo *models.PrivateMessage, userId int) (error, int) {
 	// need to check
 	var err error
-	fmt.Println("messageInfo.ReceiverID", messageInfo.ReceiverID, "messageInfo.SenderID", messageInfo.SenderID, "userId:", userId)
-	if messageInfo.ReceiverID == userId{
-		return errors.New("single conversation") ,http.StatusBadRequest
+	if messageInfo.ReceiverID == userId {
+		return errors.New("single conversation"), http.StatusBadRequest
 	}
 	err = repos.CheckUserExist(messageInfo.ReceiverID)
 	if err != nil {
 		return err, http.StatusBadRequest
 	}
 
-	err = repos.InserMessages(messageInfo,userId)
+	err = repos.InserMessages(messageInfo, userId)
 	if err != nil {
 		return err, http.StatusInternalServerError
 	}
