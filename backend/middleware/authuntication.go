df265047db9a65e25c74bc820aa790a31f275fa2
unknown <fadlo.hosaily@gmail.com>
2026-03-30 16:28:50 +0100

    .\authuntication

--- backend/middleware/authuntication.go
+++ backend/middleware/authuntication.go
@@ -11,29 +11,31 @@ type MiddlewareInfoFormat struct {
 	UserID   int
 	Nickname string
 }
-type middleWarRespons struct{
+type middleWarRespons struct {
 	Statue string `json:"statue"`
 }
 
 type Context string
+
 var Contextkey Context = "userInfo"
 
 func Authuntication(handler http.Handler) http.Handler {
 	var response middleWarRespons
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		err, session,_ := repos.CheckSession(r)
+		err, session, _ := repos.CheckSession(r)
 		if err != nil {
 			response.Statue = "Unauthorized"
-		w.Header().Set("Content-Type","application/json")
-		json.NewEncoder(w).Encode(response)
+			w.Header().Set("Content-Type", "application/json")
+			w.WriteHeader(http.StatusUnauthorized)
+			json.NewEncoder(w).Encode(response)
 			return
 		}
-		userInfo:= MiddlewareInfoFormat{
-			UserID: session.UserID,
+		userInfo := MiddlewareInfoFormat{
+			UserID:   session.UserID,
 			Nickname: session.UserNickname,
 		}
-		ctx:= context.WithValue(r.Context(),Contextkey,userInfo)
-		handler.ServeHTTP(w,r.WithContext(ctx))
+		ctx := context.WithValue(r.Context(), Contextkey, userInfo)
+		handler.ServeHTTP(w, r.WithContext(ctx))
 	})
 
 }
