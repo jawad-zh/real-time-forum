667274dd75a044af44ea1716591244422cc27496
unknown <fadlo.hosaily@gmail.com>
2026-03-30 16:29:49 +0100

    structs

--- backend/models/structs.go
+++ backend/models/structs.go
@@ -6,42 +6,41 @@ import (
 )
 
 type Users struct {
-	UserID    int
-	Nickname  string `json:"Nickname"`
-	Age       int    `json:"Age"`
-	Gender    string `json:"Gender"`
-	FirstName string `json:"FirstName"`
-	LastName  string `json:"LastName"`
-	Email     string `json:"Email"`
-	Password  string `json:"Password"`
-	CreatedAt time.Time `json:"CreatedAt"`
-	Likes int `json:"Likes"`
-	Saves int `json:"Saves"`
-	ProfileURL string `json:"ProfileURL"`
-	IsRead     sql.NullBool `json:"IsRead"`
+	UserID          int
+	Nickname        string         `json:"Nickname"`
+	Age             int            `json:"Age"`
+	Gender          string         `json:"Gender"`
+	FirstName       string         `json:"FirstName"`
+	LastName        string         `json:"LastName"`
+	Email           string         `json:"Email"`
+	Password        string         `json:"Password"`
+	CreatedAt       time.Time      `json:"CreatedAt"`
+	Likes           int            `json:"Likes"`
+	Saves           int            `json:"Saves"`
+	ProfileURL      string         `json:"ProfileURL"`
+	IsRead          sql.NullBool   `json:"IsRead"`
 	LastMessageTime sql.NullString `json:"LastMessageTime"`
-
 }
 type Session struct {
-	UserID    int `json:"UserID"`
-	Token     string `json:"Token"`
-	ExpiresAt time.Time `json:"ExpiresAt"`
+	UserID       int       `json:"UserID"`
+	Token        string    `json:"Token"`
+	ExpiresAt    time.Time `json:"ExpiresAt"`
 	UserNickname string
 }
 
 // need to add image
 type Posts struct {
-	PostID    int `json:"PostID"`
-	UserID    int `json:"UserID"`
-	Title     string `json:"Title"`
-	Content   string `json:"Content"`
-	ImageURL string `json:"ImageURL"`
-	CreatedAt string `json:"CreatedAt"`
-	Nickname string `json:"Nickname"`
+	PostID     int            `json:"PostID"`
+	UserID     int            `json:"UserID"`
+	Title      string         `json:"Title"`
+	Content    string         `json:"Content"`
+	ImageURL   string         `json:"ImageURL"`
+	CreatedAt  string         `json:"CreatedAt"`
+	Nickname   string         `json:"Nickname"`
 	ProfileURL sql.NullString `json:"ProfileURL"`
-	Categories []string `json:"Categories"`
-	Isliked int `json:"Isliked"`
-	IsSaved int `json:"IsSaved"`
+	Categories []string       `json:"Categories"`
+	Isliked    int            `json:"Isliked"`
+	IsSaved    int            `json:"IsSaved"`
 }
 type PostCategoies struct {
 	PostID   int `json:"PostID"`
@@ -53,27 +52,27 @@ type PostInteractions struct {
 	Interaction int `json:"Interaction"`
 }
 type Comments struct {
-	CommentID int `json:"CommentID"`
-	PostID    int `json:"PostID"`
-	UserID   int `json:"UserID"`
-	Content   string `json:"Content"`
-	CreatedAt string `json:"CreatedAt"`
-	UserProfile sql.NullString `json:"UserProfile"`
-	UserGender string `json:"Gender"`
-	UserNickname string `json:"Nickname"`
+	CommentID    int            `json:"CommentID"`
+	PostID       int            `json:"PostID"`
+	UserID       int            `json:"UserID"`
+	Content      string         `json:"Content"`
+	CreatedAt    string         `json:"CreatedAt"`
+	UserProfile  sql.NullString `json:"UserProfile"`
+	UserGender   string         `json:"Gender"`
+	UserNickname string         `json:"Nickname"`
 }
 type CommentInteractions struct {
-	UserId     int `json:"UserId"`
+	UserId      int `json:"UserId"`
 	CommentID   int `json:"CommentID"`
 	Interaction int `json:"Interaction"`
 }
 type PrivateMessage struct {
-	MessageID  int `json:"MessageID"`
-	SenderID   int  `json:"senderID"`
-	ReceiverID int `json:"receiverID"`
+	MessageID  int    `json:"MessageID"`
+	SenderID   int    `json:"senderID"`
+	ReceiverID int    `json:"receiverID"`
 	Content    string `json:"messageContent"`
 	CreatAt    string `json:"CreatAt"`
-	IsRead     bool `json:"IsRead"`
+	IsRead     bool   `json:"IsRead"`
 }
 
 type Login struct {
@@ -85,13 +84,13 @@ type PostInformation struct {
 	Title      string   `json:"Title"`
 	Content    string   `json:"Content"`
 	Categories []string `json:"Categories"`
-	ImageURL string `json:"ImageURL"`
+	ImageURL   string   `json:"ImageURL"`
 }
-type PostLike struct{
+type PostLike struct {
 	UserID int `json:"UserID"`
 	PostID int `json:"PostID"`
 }
-type PostSave struct{
+type PostSave struct {
 	UserID int `json:"UserID"`
 	PostID int `json:"PostID"`
 }
