e2f589a6614979542d37ee163ac0795baddc8d57
unknown <fadlo.hosaily@gmail.com>
2026-03-30 16:19:22 +0100

    save post and send message

--- backend/services/SendeMessengeService.go
+++ backend/services/SendeMessengeService.go
@@ -2,24 +2,25 @@ package services
 
 import (
 	"errors"
-	"net/http"
-
+	"fmt"
 	"golang/backend/models"
 	"golang/backend/repos"
+	"net/http"
 )
 
 func SendeMessageService(messageInfo *models.PrivateMessage, userId int) (error, int) {
 	// need to check
 	var err error
-	if messageInfo.ReceiverID == userId {
-		return errors.New("single conversation"), http.StatusBadRequest
+	fmt.Println("messageInfo.ReceiverID", messageInfo.ReceiverID, "messageInfo.SenderID", messageInfo.SenderID, "userId:", userId)
+	if messageInfo.ReceiverID == userId{
+		return errors.New("single conversation") ,http.StatusBadRequest
 	}
 	err = repos.CheckUserExist(messageInfo.ReceiverID)
 	if err != nil {
 		return err, http.StatusBadRequest
 	}
 
-	err = repos.InserMessages(messageInfo, userId)
+	err = repos.InserMessages(messageInfo,userId)
 	if err != nil {
 		return err, http.StatusInternalServerError
 	}
--- backend/services/savePostService.go
+++ backend/services/savePostService.go
@@ -2,12 +2,16 @@ package services
 
 import (
 	"fmt"
+	"net/http"
 
 	"golang/backend/repos"
 )
 
 func SavePostService(UserID int, postID int) (error, string,int) {
-
+	err:= repos.CheckExist(postID)
+	if err != nil{
+		return err , "" ,http.StatusBadRequest
+	}
 	err, message ,statueCode:= repos.SavePost(postID, UserID)
 	if err != nil {
 		fmt.Println("Erorrrrrrrrr", err)
