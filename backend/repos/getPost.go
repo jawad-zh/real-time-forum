878964c518ee33b86b7545fa3fc33596958f4653
unknown <fadlo.hosaily@gmail.com>
2026-03-30 16:38:11 +0100

    get post

--- backend/repos/getPost.go
+++ backend/repos/getPost.go
@@ -10,7 +10,7 @@ import (
 	"golang/backend/models"
 )
 
-func GetPosts(category string, r *http.Request, UserID int, offset int) (*[]models.Posts, error,int) {
+func GetPosts(category string, r *http.Request, UserID int, offset int) (*[]models.Posts, error, int) {
 	var rows *sql.Rows
 	var err error
 	switch category {
@@ -123,13 +123,12 @@ func GetPosts(category string, r *http.Request, UserID int, offset int) (*[]mode
 		`, category, offset, UserID, UserID)
 	}
 
-	
 	if rows == nil {
-		return nil, fmt.Errorf("query returned nil rows"),http.StatusOK
+		return nil, fmt.Errorf("query returned nil rows"), http.StatusOK
 	}
 	if err != nil {
 		log.Println("DB query error:", err)
-		return nil, err,http.StatusInternalServerError
+		return nil, err, http.StatusInternalServerError
 	}
 	defer rows.Close()
 
@@ -176,7 +175,7 @@ func GetPosts(category string, r *http.Request, UserID int, offset int) (*[]mode
 	}
 
 	if err := rows.Err(); err != nil {
-		return nil , err , http.StatusInternalServerError
+		return nil, err, http.StatusInternalServerError
 	}
 
 	var posts []models.Posts
@@ -185,5 +184,5 @@ func GetPosts(category string, r *http.Request, UserID int, offset int) (*[]mode
 	}
 
 	fmt.Println("this is the length of the posts", len(posts))
-	return &posts, nil , http.StatusOK
+	return &posts, nil, http.StatusOK
 }
