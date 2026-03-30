26967dd347b9658c5444ff7d7b15919934036038
unknown <fadlo.hosaily@gmail.com>
2026-03-30 16:41:11 +0100

    create cmment check

--- backend/services/creatCommentCheck.go
+++ backend/services/creatCommentCheck.go
@@ -7,20 +7,20 @@ import (
 	"net/http"
 )
 
-func CommentCheck(user middleware.MiddlewareInfoFormat, PostID int, commentContent string) (error, string,int) {
-	err:=repos.CheckExist(PostID)
-	if err != nil{
-		return err,"",http.StatusBadRequest
+func CommentCheck(user middleware.MiddlewareInfoFormat, PostID int, commentContent string) (error, string, int) {
+	err := repos.CheckExist(PostID)
+	if err != nil {
+		return err, "", http.StatusBadRequest
 	}
 	if len(commentContent) == 0 {
-		return errors.New("the comment can't be empty"), "",http.StatusBadGateway
+		return errors.New("the comment can't be empty"), "", http.StatusBadGateway
 	} else if len(commentContent) >= 1000 {
-		return errors.New("The comment to large ( More than 1000 )"), "",http.StatusBadRequest
+		return errors.New("The comment to large ( More than 1000 )"), "", http.StatusBadRequest
 	}
-	err, message ,statueCode:= repos.CreatComment(user.UserID, PostID, commentContent)
+	err, message, statueCode := repos.CreatComment(user.UserID, PostID, commentContent)
 	if err != nil {
-		return err, message,statueCode
+		return err, message, statueCode
 	}
 
-	return nil, "" , http.StatusOK
+	return nil, "", http.StatusOK
 }
