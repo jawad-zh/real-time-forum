1f9cc9f2768f04cda58d74d199fa406c2e443428
jawad-zh <zahraoui.jawad8@gmail.com>
2026-03-31 22:58:32 +0100

    fix bug

--- backend/services/UpdateMessageStateService.go
+++ backend/services/UpdateMessageStateService.go
@@ -1,15 +1,18 @@
 package services
 
 import (
+	"net/http"
+
 	"golang/backend/repos"
 	"golang/backend/wbs"
-	"net/http"
+	"fmt"
 )
 
 func UpdateMessageStateServie(UserID int, ReceiverID int) (error, int) {
-	err:=repos.CheckExist(ReceiverID)
-	if err != nil{
-		return err,http.StatusBadRequest
+	err := repos.CheckUserExist(UserID)
+	if err != nil {
+		fmt.Println("yeeeeeeeeeeeeeeeep")
+		return err, http.StatusBadRequest
 	}
 	err, statueCode := repos.UpdateMessageStateRepos(UserID, ReceiverID)
 	if err != nil {
