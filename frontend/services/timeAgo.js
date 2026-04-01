e2860724ee906a90bbf77df9213e0ea13cfe8756
jawad-zh <zahraoui.jawad8@gmail.com>
2026-04-01 19:16:22 +0100

    last v

--- frontend/services/timeAgo.js
+++ frontend/services/timeAgo.js
@@ -24,9 +24,13 @@ export function TimeAgo(createdAt) {
 export function formatTime(isoString) {
     const date = new Date(isoString);
 
+    const year = date.getFullYear();
+    const month = (date.getMonth() + 1).toString().padStart(2, "0");
+    const day = date.getDate().toString().padStart(2, "0");
+
     const hours = date.getHours().toString().padStart(2, "0");
     const minutes = date.getMinutes().toString().padStart(2, "0");
 
-    return `${hours}:${minutes}`;
+    return `${year}-${month}-${day} ${hours}:${minutes}`;
 }
 
